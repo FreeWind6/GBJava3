52af963a1d24ceb6ae85e964ded736ad901bf8a0
Dmitry <free-wind-2012@yandex.ru>
2020-02-12 17:33:53 +0300

    add home work

--- src/main/java/Lesson_5/HW/MainClass.java
+++ src/main/java/Lesson_5/HW/MainClass.java
@@ -2,22 +2,51 @@ package Lesson_5.HW;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainClass {
     public static final int CARS_COUNT = 4;
+    private static ExecutorService executorService = Executors.newFixedThreadPool(CARS_COUNT);
+    private static final AtomicInteger atomicInteger = new AtomicInteger(0);
 
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT + 1,
+                () -> System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!"));
+        CountDownLatch countDownLatch = new CountDownLatch(CARS_COUNT);
+
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
+
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cyclicBarrier, countDownLatch, atomicInteger);
         }
+
         for (int i = 0; i < cars.length; i++) {
+            executorService.execute(cars[i]);
+        }
+
+        try {
+            cyclicBarrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+            executorService.shutdown();
+        }
+/*        for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
-        }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        }*/
     }
 }
 
